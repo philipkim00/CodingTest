383d67864275c57e9b6746f2c3bfdf96390d1117
김민혁 <philipkim00@gmail.com>
2024-05-29 18:41:43 +0900

    1068번 문제 풀이

---
+++ 기초 100제/Basic/Condition_SelectionExecutionStructure/1068.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+
+	if (a >= 90)
+	{
+		cout << 'A'<< endl;
+	}
+	else if (a >= 70)
+	{
+		cout << 'B' << endl;
+	}
+	else if (a >= 40)
+	{
+		cout << 'C' << endl;
+	}
+	else
+	{
+		cout << "D" << endl;
+
+	}
+}
