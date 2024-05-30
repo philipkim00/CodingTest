93f05ddd3fe13aa4351e88265a489efad8aa31c9
김민혁 <philipkim00@gmail.com>
2024-05-30 23:09:30 +0900

    1073번 문제 풀이

---
+++ 기초 100제/Basic/RepeatExecutionStructure/1073.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    while (true)
+    {
+        cin >> num;
+
+        if (num == 0)
+        {
+            break;
+        }
+
+        cout << num << endl;
+    }
+
+    return 0;
+}
