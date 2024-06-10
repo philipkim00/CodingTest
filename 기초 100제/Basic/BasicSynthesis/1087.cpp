72925972f2da7c7f0a971735efaa46188ea3b252
김민혁 <philipkim00@gmail.com>
2024-06-10 23:42:16 +0900

    1087번 문제 풀이

--- 기초 100제/Basic/BasicSynthesis/1087.cpp
+++ 기초 100제/Basic/BasicSynthesis/1087.cpp
@@ -6,11 +6,11 @@ int main()
     int n;
     cin >> n;
 
-    int sum = 1;
+    int sum = 0;
 
     for (int i = 0; i < n; i++)
     {
-        sum += i;
+        sum += i + 1;
 
         if (sum >= n)
         {
